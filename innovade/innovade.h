dbb80766855c4f470358a583ab5a6b0959c286a3
Ryosuke Hayashi <hayashi@hayashikun.com>
2017-09-10 17:23:36 +0900

    first commit

---
+++ innovade/innovade.h
@@ -0,0 +1,19 @@
+//
+//  innovade.h
+//  innovade
+//
+//  Created by Ryosuke Hayashi on 2017/09/10.
+//  Copyright © 2017年 yagra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for innovade.
+FOUNDATION_EXPORT double innovadeVersionNumber;
+
+//! Project version string for innovade.
+FOUNDATION_EXPORT const unsigned char innovadeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <innovade/PublicHeader.h>
+
+
